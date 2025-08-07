632157baacfe69c078bd9e28ef569eb325c17d48
Marileni <94244395+Marileni@users.noreply.github.com>
2025-08-07 18:54:49 +0200

    Cleaning the repository

--- multilabel_emotion_classification/models/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Models module for multilabel emotion classification."""
-
-from .bert_model import BertForMultiLabelClassification, ModelConfig
-
-__all__ = ['BertForMultiLabelClassification', 'ModelConfig']
--- multilabel_emotion_classification/training/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Training module for multilabel emotion classification."""
-
-from .trainer import EmotionTrainer, TrainingConfig
-from .utils import setup_training, create_training_arguments
-
-__all__ = ['EmotionTrainer', 'TrainingConfig', 'setup_training', 'create_training_arguments']
